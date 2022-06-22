6c40457ca349817400c2a206adeed35619a517d2
Ruslan <gadjat@mail.ru>
2022-06-23 00:11:04 +0300

    4th-add method

--- src/main/java/com/javarush/island/bulimov/entity/animals/herbivores/Herbivore.java
+++ src/main/java/com/javarush/island/bulimov/entity/animals/herbivores/Herbivore.java
@@ -1,7 +1,15 @@
 package com.javarush.island.bulimov.entity.animals.herbivores;
 
 import com.javarush.island.bulimov.entity.animals.Animal;
+import com.javarush.island.bulimov.entity.animals.carnivores.Carnivore;
+import com.javarush.island.bulimov.exception.IslandRunException;
 import com.javarush.island.bulimov.islandMap.Cell;
+import com.javarush.island.bulimov.islandMap.IslandMapCreator;
+import com.javarush.island.bulimov.resources.AnimalsEatPercent;
+import com.javarush.island.bulimov.resources.Percent;
+
+import java.util.HashSet;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Herbivore extends Animal {
     public Herbivore(double weight, int maxItemCell, int maxSpeedInCell, double maxSaturation, double saturation) {
@@ -10,17 +18,69 @@ public class Herbivore extends Animal {
 
     @Override
     public void eating(Cell cell) {
+        for (Herbivore herbivore : cell.herbivores) {
+            if(!herbivore.hold && this.saturation != this.maxSaturation){
+                herbivore.hold = true;
+                HashSet<Percent> percents = AnimalsEatPercent.getInstance().getEatPercent().get(this.name);
+                for (Percent percent : percents) {
+                    if(herbivore.name.equals(percent.getName())){
+                        if(herbivore.weight + this.saturation >= this.maxSaturation){
+                            this.saturation = this.maxSaturation;
+                        }
+                        else{
+                            this.saturation+= herbivore.weight;
+                        }
+                        herbivore.weight = 0;
+                    }
+                }
 
-    }
+            }
+        }
+    } // переделпть под траву
 
     @Override
     public void moving(Cell cell) {
-
+        IslandMapCreator.getAnimalMap()[cell.column][cell.line].herbivores.remove(this);
+        int rout = ThreadLocalRandom.current().nextInt(0,4);
+        switch (rout){
+            case 1 -> {
+                if (cell.column+this.maxItemCell >= 100)
+                    cell.column = 100;
+                else{
+                    cell.column+=this.maxItemCell;
+                }
+            }
+            case 2 -> {
+                if (cell.column-this.maxItemCell <= 0)
+                    cell.column = 0;
+                else{
+                    cell.column-=this.maxItemCell;
+                }
+            }
+            case 3 -> {
+                if (cell.line+this.maxItemCell >= 20)
+                    cell.line = 20;
+                else{
+                    cell.line+=this.maxItemCell;
+                }
+            }
+            case 4 -> {
+                if (cell.line-this.maxItemCell <= 0)
+                    cell.line = 0;
+                else{
+                    cell.line-=this.maxItemCell;
+                }
+            }
+        }
+        IslandMapCreator.getAnimalMap()[cell.column][cell.line].herbivores.add(this);
     }
 
-    //moving перенести в Animal
     @Override
-    public void reproducing(Cell cell) {
-
+    public void reproducing(Cell cell)  {
+        try {
+            cell.herbivores.add((Herbivore) this.clone()); // дописать сюда получение класса через парсер
+        } catch (CloneNotSupportedException e) {
+            throw new IslandRunException("reproducing error "+e);
+        }
     }
 }
